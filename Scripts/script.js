f6bec95b26456c210d1590c12fed8da42dd1dc1b
samuelbsp <sambolduc@gmail.com>
2015-02-24 15:45:08 -0500

    Nouvelle grosse version. Page contenu et contenu film terminé.Manque vidéo dans contenu film.

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,36 @@
 $(document).ready(function() {
-	setTimeout(function() {
-      $( ".blocContenu" ).css( "bottom", "0px" );
-	}, 100);
+
+  var hauteurFenetre = $(window).height();	
+  $( ".backgroundContenu" ).css( "height", hauteurFenetre);
+  $( ".blocContenu" ).css( "height", hauteurFenetre-180);
+  $( ".blocContenuFilm" ).css( "bottom", - hauteurFenetre);
+  $( ".blocFilm" ).css( "top", hauteurFenetre/4.5);
+
+if($(".blocObjectif").length > 0) {
+    logoCentre();
+}
+
+setTimeout(function() {
+    $( ".blocContenu" ).css( "bottom", "0px" );
+}, 100);
+
+setTimeout(function() {
+    $( ".blocTitre" ).css( "left", "0px" );
+      	setTimeout(function() {
+      		$( ".blocContenuFilm" ).css( "bottom", "0px" );
+      			setTimeout(function() {
+      				$( ".blocVideo" ).css( "width", "50%" );
+      					setTimeout(function() {
+      						$( ".blocVote" ).css( "width", "14%" );
+						}, 600);
+				}, 300);
+		}, 300);
+}, 100);
+
+function logoCentre(){
+
+  $( "header" ).css( "height", hauteurFenetre);
+  var hauteurMargeLogo = $(window).height()/2- $( ".blocTitre" ).height()/2-80;
+  $( ".blocTitre" ).css( "margin-top", hauteurMargeLogo);
+}
 });	
